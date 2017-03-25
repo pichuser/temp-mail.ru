dfbbc6463cb21c58b55b905a3483db091a59eb85
Pichuzhkin Sergey <sp@appdaddy.com>
2017-03-26 01:20:35 +0300

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 let gulp = require('gulp');
 let ts = require('gulp-typescript');
+let clean = require('gulp-clean');
+let runSequence = require('run-sequence');
 
 gulp.task('build', function() {
     const merge = require('merge2');
@@ -12,4 +14,13 @@ gulp.task('build', function() {
             gulp.dest(tsProject.config.compilerOptions.outDir)
         )
     ]);
+});
+
+gulp.task('clean', function () {
+    return gulp.src('dist', { read: false })
+        .pipe(clean());
+});
+
+gulp.task('default', function(done) {
+    runSequence('clean', 'build', done);
 });
